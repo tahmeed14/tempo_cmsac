876f391b2e2a3c104355936b9bd221c19545fa9e
Tahmeed M Laptop <tureen@umich.edu>
2026-08-25 00:13:38 -0400

    wip: load for events

--- pipelines/process_events.py
+++ pipelines/process_events.py
@@ -1,11 +1,11 @@
 from tempoctrl.providers.gradient_sports.ingest import read_events
 from tempoctrl.providers.gradient_sports.event_transform import (
-    transform_events,
-    finalize_events
+    transform_events
     )
 from tempoctrl.providers.gradient_sports.event_features import (
-    event_features
+    features_events
     )
+from tempoctrl.providers.gradient_sports.event_load import load_events
 
 def main():
     matches = [i for i in range(10517, 10518)]
@@ -13,8 +13,8 @@ def main():
 
     df = read_events(local_path)
     df = transform_events(df)
-    df = event_features(df)
-    df = finalize_events(df)
+    df = features_events(df)
+    df = load_events(df, None, None)
     
     # df.write_parquet(file="data/investigate/10517_filterafter.parquet",
     # df.write_parquet(file="data/investigate/10517_filterbefore.parquet",
