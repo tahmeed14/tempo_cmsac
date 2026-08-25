f61ad72830e5d36617fd54c5b059875e268fdcd9
Tahmeed M Laptop <tureen@umich.edu>
2026-08-25 12:28:19 -0400

    feat: wrap the processing into a load function

---
+++ src/tempoctrl/providers/gradient_sports/event_load.py
@@ -0,0 +1,72 @@
+import polars as pl
+from pathlib import Path
+
+FINALIZE_ORDER = (
+        "gameid",
+        "pe_formattedgameclock",
+        "gamestate",
+        "ge_playername",
+        "ge_playerid",
+        "ge_teamname",
+        "ge_teamid",
+        "match_team_possession_id",
+        "match_team_player_possession_id",
+        "team_possession_start",
+        "event_number",
+        "ge_gameeventtype"
+        )
+
+FINALIZE_EXCLUDE = (
+    *FINALIZE_ORDER,
+    "ge_outtype", 
+    "ge_endtype",
+)
+
+def order_event_columns(df_in: pl.DataFrame) -> pl.DataFrame:
+    return (
+        df_in.filter(pl.col("match_team_possession_id").is_not_null())
+        .select(
+            *FINALIZE_ORDER,
+            pl.exclude(*FINALIZE_ORDER, *FINALIZE_EXCLUDE),
+        )
+    )
+
+def remove_event_prefixes(df_in : pl.DataFrame) -> pl.DataFrame:
+    return (
+        df_in.rename(
+            lambda column_name: (
+                column_name[3:]
+                if column_name.startswith(("ge_", "pe_"))
+                else column_name
+            )
+        )
+    )
+
+#TODO:
+def validate_events(df_in : pl.DataFrame) -> pl.DataFrame:
+    """"""
+
+    return -1
+
+def load_events(df_in: pl.DataFrame,
+                output_dir : str | Path,
+                match_id : str | int) -> None:
+    """
+    """
+
+    # polish
+    events_df = (df_in.pipe(order_event_columns)
+                 .pipe(remove_event_prefixes))
+
+    print(events_df)
+
+    # validate
+
+    "data/processed/gradient_sports/events"
+
+    # write
+    events_df.write_parquet(file = f'''{output_dir}{match_id}.parquet''',
+                    compression="zstd"
+                    )
+        
+    
