15bebad06f2d84f647ecedd513082025263c9670
Tahmeed M Laptop <tureen@umich.edu>
2026-08-27 00:18:15 -0400

    refactor: reorder the load order of operations

--- src/tempoctrl/gradient_sports/event_load.py
+++ src/tempoctrl/gradient_sports/event_load.py
@@ -9,35 +9,6 @@ logging.basicConfig(
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
 )
 
-FINALIZE_ORDER = (
-        "gameid",
-        "pe_formattedgameclock",
-        "gamestate",
-        "ge_playername",
-        "ge_playerid",
-        "ge_teamname",
-        "ge_teamid",
-        "match_team_possession_id",
-        "match_team_player_possession_id",
-        "team_possession_start",
-        "event_number",
-        "ge_gameeventtype"
-        )
-
-FINALIZE_EXCLUDE = (
-    *FINALIZE_ORDER,
-    "ge_outtype", 
-    "ge_endtype",
-)
-
-def order_event_columns(df_in: pl.DataFrame) -> pl.DataFrame:
-    return (
-        df_in.filter(pl.col("match_team_possession_id").is_not_null())
-        .select(
-            *FINALIZE_ORDER,
-            pl.exclude(*FINALIZE_ORDER, *FINALIZE_EXCLUDE),
-        )
-    )
 
 def remove_event_prefixes(df_in : pl.DataFrame) -> pl.DataFrame:
     return (
@@ -55,8 +26,8 @@ RENAME_MAPPER = {
     "gameid" : "game_id",
     "gameeventid" : "game_event_id",
     "possessioneventid" : "possession_event_id",
-    "gameeventtype" : "game_event_type",
-    "possessioneventtype" : "possession_event_type"
+    "ge_gameeventtype" : "game_event_type",
+    "pe_possessioneventtype" : "possession_event_type"
 
     }
 
@@ -69,6 +40,37 @@ def validate_events(df_in : pl.DataFrame) -> pl.DataFrame:
 
     return -1
 
+FINALIZE_ORDER = (
+        "game_id",
+        "formattedgameclock",
+        "gamestate",
+        "playername",
+        "playerid",
+        "teamname",
+        "teamid",
+        "match_team_possession_id",
+        "match_team_player_possession_id",
+        "team_possession_start",
+        "event_number",
+        "game_event_type"
+        )
+
+FINALIZE_EXCLUDE = (
+    *FINALIZE_ORDER,
+    "outtype",
+    "endtype",
+)
+
+
+def organize_event_columns(df_in: pl.DataFrame) -> pl.DataFrame:
+    return (
+        df_in.filter(pl.col("match_team_possession_id").is_not_null())
+        .select(
+            *FINALIZE_ORDER,
+            pl.exclude(*FINALIZE_ORDER, *FINALIZE_EXCLUDE),
+        )
+    )
+
 def load_events(df_in: pl.DataFrame,
                 output_dir : str | Path,
                 match_id : str | int) -> None:
@@ -77,9 +79,9 @@ def load_events(df_in: pl.DataFrame,
 
     # polish
     events_df = (df_in
-                 .pipe(order_event_columns)
+                 .pipe(rename_columns)
                  .pipe(remove_event_prefixes)
-                 .pipe(rename_columns))
+                 .pipe(organize_event_columns))
 
     logger.debug(events_df)
 
