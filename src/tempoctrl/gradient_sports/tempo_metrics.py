dd7b5f45ea0e0b9bdbefe4e833471e70dba69287
Tahmeed M Laptop <tureen@umich.edu>
2026-08-30 20:57:25 -0400

    feat: successful delivery end frame ownership given to passer and not receiver

--- src/tempoctrl/gradient_sports/tempo_metrics.py
+++ src/tempoctrl/gradient_sports/tempo_metrics.py
@@ -6,6 +6,11 @@ import polars as pl
 
 FPS = 29.97  # FIXME: Pull from game metadata.
 
+SYNTHETIC_PASS_END_COLUMN = "is_synthetic_pass_end"
+
+_IS_METRIC_FRAME_COLUMN = "__is_ball_metric_frame"
+_METRIC_INPUT_ORDER_COLUMN = "__ball_metric_input_order"
+
 
 def _resolve_group_columns(
     df: pl.LazyFrame,
@@ -40,6 +45,40 @@ def _resolve_group_columns(
     return group_columns
 
 
+def _add_metric_frame_marker(
+    df: pl.LazyFrame,
+    group_columns: tuple[str, ...],
+) -> pl.LazyFrame:
+    """Mark one metric row per frame, preferring a synthetic pass row."""
+    schema = df.collect_schema()
+    frame_columns = (*group_columns, "framenum")
+    input_order = pl.col(_METRIC_INPUT_ORDER_COLUMN)
+    is_synthetic = (
+        pl.col(SYNTHETIC_PASS_END_COLUMN).fill_null(False)
+        if SYNTHETIC_PASS_END_COLUMN in schema
+        else pl.lit(False)
+    )
+
+    with_input_order = df.with_columns(
+        pl.int_range(pl.len()).alias(_METRIC_INPUT_ORDER_COLUMN)
+    )
+    first_input_order = input_order.min().over(frame_columns)
+    first_synthetic_order = (
+        pl.when(is_synthetic)
+        .then(input_order)
+        .otherwise(None)
+        .min()
+        .over(frame_columns)
+    )
+
+    return with_input_order.with_columns(
+        (
+            input_order
+            == pl.coalesce(first_synthetic_order, first_input_order)
+        ).alias(_IS_METRIC_FRAME_COLUMN)
+    )
+
+
 def add_ball_displacement(
     df: pl.LazyFrame,
     possession_groups: str | Sequence[str],
@@ -47,34 +86,49 @@ def add_ball_displacement(
     """Add frame-ordered 2D displacement within each possession."""
     group_columns = _resolve_group_columns(df, possession_groups)
     ball_struct = "balls_smooth"
+    order_columns = ("framenum", _METRIC_INPUT_ORDER_COLUMN)
 
     x = pl.col(ball_struct).struct.field("x")
     y = pl.col(ball_struct).struct.field("y")
+    is_metric_frame = pl.col(_IS_METRIC_FRAME_COLUMN)
+    metric_x = pl.when(is_metric_frame).then(x)
+    metric_y = pl.when(is_metric_frame).then(y)
+    metric_frame = pl.when(is_metric_frame).then(pl.col("framenum"))
 
-    previous_x = x.shift().over(
+    previous_x = metric_x.forward_fill().shift().over(
         group_columns,
-        order_by="framenum",
+        order_by=order_columns,
     )
-    previous_y = y.shift().over(
+    previous_y = metric_y.forward_fill().shift().over(
         group_columns,
-        order_by="framenum",
+        order_by=order_columns,
     )
-    previous_frame = pl.col("framenum").shift().over(
+    previous_frame = metric_frame.forward_fill().shift().over(
         group_columns,
-        order_by="framenum",
+        order_by=order_columns,
     )
-    delta_x = x - previous_x
-    delta_y = y - previous_y
-    delta_frame = pl.col("framenum") - previous_frame
+    delta_x = pl.when(is_metric_frame).then(x - previous_x)
+    delta_y = pl.when(is_metric_frame).then(y - previous_y)
+    delta_frame = pl.when(is_metric_frame).then(
+        pl.col("framenum") - previous_frame
+    )
 
-    return df.with_columns(
-        delta_x.alias("delta_x"),
-        delta_y.alias("delta_y"),
-        delta_frame.alias("delta_frame"),
-        (
-            delta_x.pow(2) + delta_y.pow(2)
-        ).sqrt().alias("ball_displacement"),
-    ).pipe(add_ball_speed, FPS)
+    return (
+        df.pipe(_add_metric_frame_marker, group_columns)
+        .with_columns(
+            delta_x.alias("delta_x"),
+            delta_y.alias("delta_y"),
+            delta_frame.alias("delta_frame"),
+            (
+                delta_x.pow(2) + delta_y.pow(2)
+            ).sqrt().alias("ball_displacement"),
+        )
+        .drop(
+            _IS_METRIC_FRAME_COLUMN,
+            _METRIC_INPUT_ORDER_COLUMN,
+        )
+        .pipe(add_ball_speed, FPS)
+    )
 
 
 def add_ball_speed(
